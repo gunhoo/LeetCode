389026662cb5fac05aa0e3e76e0c0a756e1b3a49
Gunhoo Park <gunhoo2016@naver.com>
2023-07-19 00:04:39 +0900

    Time: 57 ms (40.81%), Space: 43.6 MB (82.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i=0; i < n-1; i++) {
-            for (int j=i+1; j < n; j++) {
-                if (nums[i]+nums[j] == target) {
-                    return new int[]{i, j};
+        int answer[] = new int[2], a = 0, b = 0;
+        out: for(int i = 0; i < nums.length-1; i++){
+            int tmpA = nums[i], tmpB;
+            for(int j = i+1; j < nums.length; j++){
+                tmpB = nums[j];
+                if(tmpA+tmpB == target) {
+                    a = i; b = j;
+                    break out;
                 }
             }
         }
-        return new int[]{}; 
+        answer[0] = a; answer[1] = b;
+        return answer;
     }
 }
