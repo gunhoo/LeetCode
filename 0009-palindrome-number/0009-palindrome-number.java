c2faa35539155fe38b6e234166b0dc3244be585f
Gunhoo Park <gunhoo2016@naver.com>
2023-07-24 23:12:34 +0900

    Time: 9 ms (99.63%), Space: 42.9 MB (72.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,8 +3,7 @@ class Solution {
         if(x < 0 || x != 0 && x % 10 == 0) return false;
         else if(x == 0) return true;
         
-        int idx = x;
-        int reserved = 0;
+        int idx = x, reserved = 0;
         
         while(idx != 0){
             reserved = (reserved * 10) + (idx % 10);
