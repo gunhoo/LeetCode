c11e3bd1ab47391e43955ad7938d358079360295
Gunhoo Park <gunhoo2016@naver.com>
2023-08-07 23:55:51 +0900

    Time: 1 ms (98.81%), Space: 40.5 MB (6.16%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,12 +1,10 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
-        for(int a : stones){
-            pq.offer(a);
+         Arrays.sort(stones);
+        for(int i=stones.length-1; i>0; i--) {
+          stones[i-1] = stones[i] - stones[i-1];
+          Arrays.sort(stones);
         }
-        while(pq.size() > 1){
-            pq.offer(pq.poll()-pq.poll());
-        }
-        return pq.poll();
+        return stones[0];
     }
 }
