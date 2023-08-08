fe2d9666ebb87307300d270e8e12bebfd2c80ec6
Gunhoo Park <gunhoo2016@naver.com>
2023-08-09 00:00:40 +0900

    Time: 19 ms (32.32%), Space: 43.8 MB (35.48%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -3,20 +3,14 @@ class Solution {
         paragraph = paragraph.toLowerCase();
         for(String ban : banned) paragraph = paragraph.replaceAll(ban, "");
         String[] symbols = {"!", "?", "'", ",", ";", "."};
-        System.out.println("--after ban--");
-        System.out.println(paragraph);
         
         paragraph = paragraph.replaceAll("[!?',;.]", " ");
-        System.out.println("--after removing symbols--");
-        System.out.println(paragraph);
         
         String[] split = paragraph.trim().replaceAll("\\s+", " ").split(" ");
-        System.out.println("--after--");
         
         Map<String, Integer> map = new HashMap<>();
         for(String s : split){
             if(s.equals(" ")) continue;
-            System.out.println(s);
             if(map.containsKey(s)){
                 map.put(s, map.get(s)+1);
             }else{
