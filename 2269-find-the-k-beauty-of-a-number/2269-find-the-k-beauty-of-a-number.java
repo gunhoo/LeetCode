ba8b8dccd0a1f669dfacc8cf7178579bc0bf685c
Gunhoo Park <gunhoo2016@naver.com>
2023-09-04 23:13:23 +0900

    Time: 1 ms (77.41%), Space: 39.3 MB (55.05%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -3,8 +3,7 @@ class Solution {
         String stringNum = ""+num;
         int ans = 0;
         for(int i = 0; i <= stringNum.length()-k ; i++){
-            if(Integer.parseInt(stringNum.substring(i, i+k)) == 0 )continue;
-            if(num % Integer.parseInt(stringNum.substring(i, i+k)) == 0) ans++;
+            if(Integer.parseInt(stringNum.substring(i, i+k)) != 0 && num % Integer.parseInt(stringNum.substring(i, i+k)) == 0) ans++;
         }
         return ans;
     }
