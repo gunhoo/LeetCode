9cefd0e105f108f6a7217f6a31e5b29a2dba517f
Gunhoo Park <gunhoo2016@naver.com>
2023-09-11 23:47:16 +0900

    Time: 4 ms (60.65%), Space: 43.7 MB (22.20%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,16 +1,21 @@
 class Solution {
-    public int thirdMax(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        Arrays.sort(nums);
-        if(nums.length == 2) return nums[1];
-        int before = nums[0], cnt = 0;
-        for(int i = nums.length-1 ;i >=  1 ; i--){
-            if(nums[i-1] != nums[i]) {
-                before = nums[i];
-                if(++cnt == 3) return before;
+     public int thirdMax(int[] nums) {
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
             }
         }
-        if(cnt == 1 ) return nums[nums.length-1];
-        return nums[0];
+        return max3 == null ? max1 : max3;
     }
 }
