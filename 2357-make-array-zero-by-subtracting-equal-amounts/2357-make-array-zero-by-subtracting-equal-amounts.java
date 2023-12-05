1e1fb517b504fa2f2bf7db75ac840608ce64342a
Gunhoo Park <gunhoo2016@naver.com>
2023-12-05 22:53:36 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (30.21%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,21 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int n = nums.length;
-        if(n == 1) {
-            if(nums[0] == 0) return 0;
-            else return 1;
-        }
-        
-        int prev = -1;
-        Arrays.sort(nums);
-        if(nums[0] == 0 ) n = -1;
-        else n = 0;
-        for(int i =0 ; i < nums.length; i++){
-            if(prev != nums[i]){
-                n++;
-                prev = nums[i];
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(num > 0){
+                set.add(num);
             }
         }
-        return n;
+        return set.size();
     }
 }
