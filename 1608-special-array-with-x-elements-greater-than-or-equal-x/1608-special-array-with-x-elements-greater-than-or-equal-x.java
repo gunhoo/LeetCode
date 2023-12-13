fa914389e48131ba87bca8ab8131afd51d5d2f09
Gunhoo Park <gunhoo2016@naver.com>
2023-12-13 23:09:16 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (41.61%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -12,12 +12,25 @@ class Solution {
         // }
         // return -1;
         
-        for(int x = 1; x <= size; x++){
-            int tmp = 0;
-            for (int i = 0 ; i < size; i++) {
-                if(nums[i] >= x) tmp++;
-            }
-            if(x == tmp) return tmp;
+        // for(int x = 1; x <= size; x++){
+        //     int tmp = 0;
+        //     for (int i = 0 ; i < size; i++) {
+        //         if(nums[i] >= x) tmp++;
+        //     }
+        //     if(x == tmp) return tmp;
+        // }
+        // return -1;
+        
+        int[] count = new int[size+1];
+        for (int i = 0; i < size; i++) {
+            if(nums[i] >= size) count[size]++;
+            else count[nums[i]]++;
+        }
+        
+        int sum = 0;
+        for (int i = size ; i > 0 ; i--) {
+            sum += count[i];
+            if(sum == i) return i;
         }
         return -1;
     }
