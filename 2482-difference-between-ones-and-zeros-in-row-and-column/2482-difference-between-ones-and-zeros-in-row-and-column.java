20f77f9c85def8a5260e1bde301ae7122fe0af20
Gunhoo Park <gunhoo2016@naver.com>
2023-12-14 16:10:59 +0900

    Time: 12 ms (48.54%), Space: 71.3 MB (82.20%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,24 +1,44 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
-        int[][] diff = new int[grid.length][grid[0].length];
-        int[] rows = new int[grid.length];
-        int[] cols = new int[grid[0].length];
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[i].length; j++) {
-                rows[i] += grid[i][j] == 1 ? 1: -1;
-            }
-        }
-        for (int i=0; i<grid[0].length; i++) {
-            for(int j=0; j<grid.length; j++) {
-                cols[i] += grid[j][i] == 1? 1 : -1;
+//         int[][] diff = new int[grid.length][grid[0].length];
+//         int[] rows = new int[grid.length];
+//         int[] cols = new int[grid[0].length];
+//         for (int i=0; i<grid.length; i++) {
+//             for (int j=0; j<grid[i].length; j++) {
+//                 rows[i] += grid[i][j] == 1 ? 1: -1;
+//             }
+//         }
+//         for (int i=0; i<grid[0].length; i++) {
+//             for(int j=0; j<grid.length; j++) {
+//                 cols[i] += grid[j][i] == 1? 1 : -1;
+//             }
+//         }
+        
+//         for (int i=0; i<diff.length; i++) {
+//             for(int j=0; j<diff[0].length; j++) {
+//                 diff[i][j] = rows[i] + cols[j];
+//             }
+//         }
+//         return diff;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] diff = new int[m][n];
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                rows[i] += grid[i][j];
+                cols[j] += grid[i][j];
             }
         }
         
-        for (int i=0; i<diff.length; i++) {
-            for(int j=0; j<diff[0].length; j++) {
-                diff[i][j] = rows[i] + cols[j];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                diff[i][j] = 2*rows[i] + 2*cols[j] - n - m;
             }
         }
+        
         return diff;
     }
 }
