6765591a07d7b0a913fa7616cc9ead43fee5e5fd
Gunhoo Park <gunhoo2016@naver.com>
2023-12-14 16:13:08 +0900

    Time: 4 ms (98.71%), Space: 73.1 MB (51.13%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,28 +1,7 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
-//         int[][] diff = new int[grid.length][grid[0].length];
-//         int[] rows = new int[grid.length];
-//         int[] cols = new int[grid[0].length];
-//         for (int i=0; i<grid.length; i++) {
-//             for (int j=0; j<grid[i].length; j++) {
-//                 rows[i] += grid[i][j] == 1 ? 1: -1;
-//             }
-//         }
-//         for (int i=0; i<grid[0].length; i++) {
-//             for(int j=0; j<grid.length; j++) {
-//                 cols[i] += grid[j][i] == 1? 1 : -1;
-//             }
-//         }
-        
-//         for (int i=0; i<diff.length; i++) {
-//             for(int j=0; j<diff[0].length; j++) {
-//                 diff[i][j] = rows[i] + cols[j];
-//             }
-//         }
-//         return diff;
         int m = grid.length;
         int n = grid[0].length;
-        int[][] diff = new int[m][n];
         int[] rows = new int[m];
         int[] cols = new int[n];
         
@@ -35,10 +14,10 @@ class Solution {
         
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                diff[i][j] = 2*rows[i] + 2*cols[j] - n - m;
+                grid[i][j] = 2*rows[i] + 2*cols[j] - n - m;
             }
         }
         
-        return diff;
+        return grid;
     }
 }
