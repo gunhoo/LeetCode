73c59dbd7efca28130b28735f1193fe2b9117ac3
Gunhoo Park <gunhoo2016@naver.com>
2024-01-08 15:35:37 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (43.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,19 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0) return "";
-        else if(strs.length == 1 || strs[0].equals("")) return strs[0];
         String prefix = strs[0];
-        for (int i = 1; i < strs.length ; i++) {
-            if(strs[i].equals("")) return "";
-            StringBuilder answer = new StringBuilder();
-            int c = 0;
-            while(prefix.charAt(c) == strs[i].charAt(c) ){
-                answer.append(strs[i].charAt(c));
-                c++;
-                if(c >= prefix.length() || c >= strs[i].length()) break;
+        for (int i=1; i<strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.isEmpty()) return "";
             }
-            if( c == 0) return "";
-            else prefix = answer.toString();
         }
         return prefix;
     }
