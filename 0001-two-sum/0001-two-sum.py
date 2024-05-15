396f7ebf9b9553d9f83e8d18e563332323a77f0e
Gunhoo Park <gunhoo2016@gmail.com>
2024-05-14 20:18:12 -0400

    Time: 1709 ms (18.45%), Space: 17.4 MB (84.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        answer = []
         for idx, val in enumerate(nums):
             for index, value in enumerate(nums[idx+1:], idx+1):
                 if(val + value == target):
-                    answer.append(idx)
-                    answer.append(index)
-                    return answer
+                    return [idx, index]
