bc4fc6407021c005789a3f1517d3fdf415e44b93
Gunhoo Park <gunhoo2016@gmail.com>
2024-06-04 16:13:40 -0400

    Time: 2 ms (63.34%), Space: 42.4 MB (13.72%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,39 +1,29 @@
 class Solution {
     public boolean isPathCrossing(String path) {
-        List<Point> set = new LinkedList<>();
-        set.add(new Point(0,0));
-        int[] idx = {0,0};
-        for (int i=0; i<path.length(); i++){
-            switch(path.charAt(i)){
-                case 'N':
-                    idx[1] += 1;
-                    break;
-                case 'W':
-                    idx[0] -= 1;
-                    break;
-                case 'E':
-                    idx[0] += 1;
-                    break;
-                case 'S':
-                    idx[1] -= 1;
-                    break;
-            }
+        Map<Character, Pair<Integer, Integer>> map = new HashMap<>();
+        map.put('N', new Pair(0, 1));
+        map.put('S', new Pair(0, -1));
+        map.put('W', new Pair(-1, 0));
+        map.put('E', new Pair(1, 0));
+        
+        Set<Pair<Integer, Integer>> set = new HashSet();
+        set.add(new Pair(0,0));
+        
+        int idx[] = {0,0};
+        
+        for (Character c : path.toCharArray()){
+            Pair<Integer, Integer> dir = map.get(c);
+            int dx = dir.getKey();
+            int dy = dir.getValue();
             
-            for (Point p : set){
-                if (p.x == idx[0] && p.y == idx[1]) {
-                    return true;
-                }
-            }
-            set.add(new Point(idx[0], idx[1]));
+            idx[0] += dx;
+            idx[1] += dy;
+            
+            Pair<Integer, Integer> pair = new Pair(idx[0], idx[1]);
+            if(set.contains(pair)) return true;
+            set.add(pair);
         }
+        
         return false;
-    }
-    
-    class Point{
-        int x, y;
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
     }
 }
