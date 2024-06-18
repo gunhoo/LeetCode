94a18d7120f28b45662ac6d6b35199c3351b4e12
Gunhoo Park <gunhoo2016@gmail.com>
2024-06-18 13:34:39 -0400

    Time: 9 ms (32.91%), Space: 47.2 MB (97.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new LinkedList<>();
-        int[][] map = new int[strs.length][26];
-        for(int i=0; i<strs.length; i++){
-            String s = strs[i];
-            for(int j=0; j<s.length(); j++){
-                map[i][s.charAt(j)-'a']++;
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String afterSort = new String(charArray);
+            
+            if(!map.containsKey(afterSort)){
+                map.put(afterSort, new LinkedList<>());
             }
+            
+            map.get(afterSort).add(s);
         }
-        boolean[] visited = new boolean[strs.length];
-        for(int i=0; i<strs.length; i++){
-            if(visited[i]) continue;
-            List<String> list = new LinkedList<>();
-            list.add(strs[i]);
-            for(int j=i+1; j<strs.length; j++){
-                if(visited[j] == false && isEqualArray(map[i], map[j])){
-                    visited[j] = true;
-                    list.add(strs[j]);
-                }
-            }
-            ans.add(list);
-        }
-        return ans;
-    }
-    
-    private boolean isEqualArray(int[] a, int[] b){
-        for(int i=0; i<a.length; i++){
-            if(a[i] != b[i]) return false;
-        }
-        return true;
+        
+        return new ArrayList<>(map.values());
     }
 }
