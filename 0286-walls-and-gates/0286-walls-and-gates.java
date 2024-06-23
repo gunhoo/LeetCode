742829ced9c3abfda6644a49c1db7210669bc316
Gunhoo Park <gunhoo2016@gmail.com>
2024-06-23 11:22:07 -0400

    Time: 128 ms (7.54%), Space: 161.6 MB (5.75%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -14,7 +14,6 @@ class Solution {
         for(int i=0; i<rooms.length; i++){
             for(int j=0; j<rooms[i].length; j++){
                 if(rooms[i][j]== 0){
-                    makeOne(i, j, rooms);
                     q.add(new Point(i, j));
                 } 
             }
@@ -33,11 +32,5 @@ class Solution {
         }
     }
     
-    private void makeOne(int x, int y, int[][] rooms){
-        if(x-1>=0 && rooms[x-1][y] == 2147483647) rooms[x-1][y] =1;
-        if(x+1<rooms.length && rooms[x+1][y] == 2147483647) rooms[x+1][y] = 1;
-        if(y-1>=0 && rooms[x][y-1] == 2147483647) rooms[x][y-1] = 1;
-        if(y+1<rooms[0].length && rooms[x][y+1] == 2147483647) rooms[x][y+1] = 1;
-    }
     
 }
