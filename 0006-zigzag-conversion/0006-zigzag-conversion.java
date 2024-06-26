05a58cfa91f02696e86d986f6387dfef8a292131
Gunhoo Park <gunhoo2016@gmail.com>
2024-06-26 12:33:42 -0400

    Time: 5 ms (70.46%), Space: 45.4 MB (32.51%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,13 +1,13 @@
 class Solution {
     public String convert(String s, int numRows) {
         if(numRows == 1) return s;
-        String[] ans = new String[numRows];
+        StringBuilder[] ans = new StringBuilder[numRows];
         boolean down = true;
         int iterator = 0;
-        for(int i=0; i<numRows; i++) ans[i] = "";
+        for(int i=0; i<numRows; i++) ans[i] = new StringBuilder();
         for(int i=0; i<s.length(); i++){
             if(down){
-                ans[iterator] += s.charAt(i);
+                ans[iterator].append(s.charAt(i));
                 if(iterator == numRows-1){
                     down = false;
                     iterator -= 1;
@@ -15,7 +15,7 @@ class Solution {
                     iterator += 1;
                 }
             } else {
-                ans[iterator] += s.charAt(i);
+                ans[iterator].append(s.charAt(i));
                 if(iterator == 0){
                     down = true;
                     iterator += 1;
@@ -26,7 +26,7 @@ class Solution {
         }
         StringBuilder answer = new StringBuilder();
         for(int i=0; i<numRows; i++){
-            answer.append(ans[i]);
+            answer.append(ans[i].toString());
         }
         return answer.toString();
     }
