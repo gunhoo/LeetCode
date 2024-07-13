6be6ab297c48540e9be27979a9554404241eb93c
Gunhoo Park <gunhoo2016@gmail.com>
2024-07-13 18:35:17 -0400

    Time: 15 ms (94.53%), Space: 44.2 MB (48.36%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,45 +1,17 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        int[][] min = new int[n][n];
-        int[][] max = new int[n][n];
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
-                if(i==j){
-                    min[i][j] = nums[i];
-                    max[i][j] = nums[i];
-                } else {
-                    min[i][j] = 2000000003;
-                    max[i][j] = -2000000003;
-                }
-            }
-        }
-        
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                min[i][j] = Math.min(min[i][j-1], nums[j]);
-                max[i][j] = Math.max(max[i][j-1], nums[j]);
-            }
-        }
-        
         long answer = 0;
+        
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(min[i][j] == 2000000003 || max[i][j] == -2000000003 || i==j) continue;
-                answer += (max[i][j] - min[i][j]);
+            int min = nums[i], max = nums[i];
+            for(int j=i; j<n; j++){
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+                answer += max - min;
             }
         }
         
         return answer;
     }
 }
-
-// i부터 j까지 max/min 은 Max
-// MAX(i,j) = MAX(MAX(i,j-1), j)
-// MIN(i,j) = MIN(MIN(i, j-1), j)
-    
-    
-//     2차원 DP 만들어야겠네
-    
-    
