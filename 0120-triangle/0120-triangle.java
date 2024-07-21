04f2173bf06179eb21d05801288f202839ad6cde
Gunhoo Park <gunhoo2016@gmail.com>
2024-07-21 15:50:57 -0400

    Time: 6 ms (25.86%), Space: 45.7 MB (5.92%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        List<Integer> prevRow = triangle.get(0);
+        for(int row=1; row<triangle.size(); row++){
+            List<Integer> currRow = new ArrayList<>();
+            for(int col=0; col<=row; col++){
+                int smallestAbove = Integer.MAX_VALUE;
+                if(col>0) smallestAbove = prevRow.get(col-1);
+                if(col<row) smallestAbove = Math.min(smallestAbove, prevRow.get(col));
+                currRow.add(smallestAbove+triangle.get(row).get(col));
+            }
+            prevRow = currRow;
+        }
+        return Collections.min(prevRow);
+    }
+}
