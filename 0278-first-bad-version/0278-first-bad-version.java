3b40f3a618f2db9596411764f64c906982bd9436
Gunhoo Park <gunhoo2016@gmail.com>
2025-02-12 18:00:34 -0800

    Time: 25 ms (70.97%), Space: 40.6 MB (37.58%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,21 +3,17 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        // Binary Search
-        if(n == 1) return 1;
-        return findFirstBadVersion(1, n);
-    }
-
-    private int findFirstBadVersion(int left, int right){
-        int mid = (int)(((long)left + (long)right) / 2);
-        if(mid == left ){
-            if(isBadVersion(mid)) return mid;
-            return right;
+        // Binary Search using while loop
+        if(n==1) return 1;
+        int start = 1, end = n, mid = 0;
+        while(start < end){
+            mid = start + (end-start)/2;
+            if(isBadVersion(mid)){
+                end = mid;
+            } else {
+                start = mid+1;
+            }
         }
-        if(isBadVersion(mid)){
-            if(isBadVersion(mid-1)) return findFirstBadVersion(left, mid-1);
-            return mid;
-        }
-        return findFirstBadVersion(mid+1, right);
+        return end;
     }
 }
