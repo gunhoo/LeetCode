7c57e95c7964c161483e6298b70e09b65e03ec6f
Gunhoo Park <gunhoo2016@gmail.com>
2025-10-26 16:27:53 -0700

    Time: 8 ms (63.43%), Space: 41.3 MB (87.31%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.java
+++ 0830-largest-triangle-area/0830-largest-triangle-area.java
@@ -2,27 +2,16 @@ class Solution {
     public double largestTriangleArea(int[][] points) {
         double ans = 0;
         for(int i=0; i<points.length; i++){
+            int[] a = points[i]; 
             for(int j=i+1; j<points.length; j++){
+                int[] b = points[j];
                 for(int k=j+1; k<points.length; k++){
-                    double a = calculateLength(points[i], points[j]);
-                    double b = calculateLength(points[j], points[k]);
-                    double c = calculateLength(points[i], points[k]);
-                    if(isValidTriangle(a,b,c)){
-                        double s = (a+b+c)/2;
-                        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-                        ans = Math.max(area, ans);
-                    }
+                    int[] c = points[k];
+                    double area = Math.abs(a[0]*b[1]+b[0]*c[1]+c[0]*a[1]-a[1]*b[0]-b[1]*c[0]-c[1]*a[0]) * 0.5;
+                    ans = Math.max(ans, area);
                 }
             }
         }
         return ans;
-    }
-
-    private double calculateLength(int[] a, int[] b){
-        return Math.sqrt((b[0]-a[0])*(b[0]-a[0]) + (b[1]-a[1])*(b[1]-a[1]));
-    }
-
-    private boolean isValidTriangle(double a, double b, double c){
-        return a+b>c && b+c>a && c+a>b;
     }
 }
