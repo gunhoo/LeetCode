b945d67e132431690024e0f250b510736f6d45d6
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-03 13:21:42 -0800

    Time: 18 ms (12.21%), Space: 51.7 MB (5.07%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
@@ -1,42 +1,43 @@
 class Solution {
     public int countPaths(int n, int[][] roads) {
+        int[] pathCounts = new int[n];
+        long[] dist = new long[n];
         List<List<int[]>> map = new ArrayList<>();
+
         for(int i=0; i<n; i++){
             map.add(new ArrayList<>());
+            dist[i] = Long.MAX_VALUE;
         }
+
         for(int[] r : roads){
             map.get(r[0]).add(new int[]{r[1], r[2]});
-            map.get(r[1]).add(new int[]{r[0], r[2]});
+            map.get(r[1]).add(new int[]{r[0], r[2]});   
         }
 
-        long[] dist = new long[n];
-        Arrays.fill(dist, Long.MAX_VALUE);
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a->a[1])); // node, distance
+        pq.add(new long[]{0,0});
         dist[0] = 0;
-
-        int[] paths = new int[n];
-        paths[0] = 1;
-
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[1]));
-        pq.offer(new long[]{0,0});
+        pathCounts[0] = 1;
 
         while(!pq.isEmpty()){
             long[] curr = pq.poll();
             int node = (int)curr[0];
-            long dis = curr[1];
-            if(dis > dist[node]) continue;
-            for(int[] neighbor : map.get(node)){
-                int nextNode = neighbor[0];
-                int time = neighbor[1];
+            long d = curr[1];
+            
+            List<int[]> neighbors = map.get(node);
 
-                if(dist[node] + time < dist[nextNode]){
-                    dist[nextNode] = dist[node] + time;
-                    paths[nextNode] = paths[node];
-                    pq.offer(new long[]{nextNode, dist[nextNode]});
-                } else if (dist[node] + time == dist[nextNode]){
-                    paths[nextNode] = (paths[nextNode]+paths[node])%1000000007;
+            for(int[] neighbor : neighbors){
+                int next  = neighbor[0];
+                int dis = neighbor[1];
+                if(dist[next] > dis + d){
+                    dist[next] = dis + d;
+                    pathCounts[next] = pathCounts[node];
+                    pq.offer(new long[]{next, dist[next]});
+                } else if (dist[next] == dis + d){
+                    pathCounts[next] = (pathCounts[next] + pathCounts[node])%1000000007;
                 }
             }
         }
-        return paths[n-1];
+        return pathCounts[n-1];
     }
 }
