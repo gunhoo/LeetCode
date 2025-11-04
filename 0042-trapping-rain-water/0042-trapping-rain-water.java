8768fb85bf2a4f87e00a69d897c6865d78c72947
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-04 14:39:39 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,16 +1,18 @@
 class Solution {
     public int trap(int[] height) {
         int answer = 0;
-        for(int i=0; i<height.length; i++){
-            int leftMax=0, rightMax=0;
+        int size = height.length;
+        for(int i=1; i< size-1; i++){
+            int leftMax = 0, rightMax = 0;
             for(int j=i; j>=0; j--){
                 leftMax = Math.max(leftMax, height[j]);
             }
-            for(int j=i; j<height.length; j++){
+            for(int j=i; j<size; j++){
                 rightMax = Math.max(rightMax, height[j]);
             }
-            answer += Math.min(rightMax, leftMax) - height[i];
+            answer += Math.min(leftMax, rightMax) - height[i];
         }
         return answer;
+            
     }
 }
