2e4d70f91c90b91a18c8da866264fac857a6d169
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-04 16:22:55 -0800

    Time: 4 ms (2.55%), Space: 94.4 MB (5.98%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,12 +4,10 @@ class Solution {
         int min=prices[0], max=prices[0];
         int candidate = 0;
         for(int i=1; i<prices.length; i++){
+            candidate = Math.max(candidate, prices[i]-min);
             if(prices[i]<prices[i-1]){
-                candidate = Math.max(candidate, prices[i]-min);
                 min = Math.min(min, prices[i]);
-            } else {
-                candidate = Math.max(candidate, prices[i]-min);
-            }
+            } 
         }
         return candidate;
     }
