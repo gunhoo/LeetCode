3717415bc6f623b1304418980d8cb7bda9a3651b
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-07 23:58:21 -0800

    Time: 10 ms (18.61%), Space: 48.9 MB (7.71%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
@@ -16,41 +16,28 @@
 class Solution {
     public int maxLevelSum(TreeNode root) {
         // Map that stores <level, sum of the level>
-        // Queue that scores a class that contaisn TreeNode, level
+        // Stack that scores a class that contaisn TreeNode
         // while q is not empty, poll TreeLevel and update the map
         // iterate all map, find the biggest sum and return that level
-        int maxSumLevel = 1;
-        Map<Integer, Integer> map = new HashMap<>();
-        Queue<TreeLevel> q = new LinkedList<>();
-
-        q.add(new TreeLevel(root, 1));
-
+        int maxSumLevel = 1, maxSum = root.val;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int currLevel = 0;
         while(!q.isEmpty()){
-            TreeLevel tl = q.poll();
-            map.put(tl.level, map.getOrDefault(tl.level, 0)+tl.tree.val);
-            if(tl.tree.left != null){
-                q.add(new TreeLevel(tl.tree.left, tl.level+1));
+            int currStackSize = q.size();
+            int currLevelSum = 0;
+            currLevel += 1;
+            for(int i=0; i<currStackSize; i++){
+                TreeNode node = q.poll();
+                currLevelSum += node.val;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
-            if(tl.tree.right != null){
-                q.add(new TreeLevel(tl.tree.right, tl.level+1));
-            }
-        }
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getValue() > map.get(maxSumLevel)){
-                maxSumLevel = entry.getKey();
+            if(currLevelSum > maxSum){
+                maxSum = currLevelSum;
+                maxSumLevel = currLevel;
             }
         }
         return maxSumLevel;
-    }
-
-    class TreeLevel{
-        TreeNode tree;
-        int level;
-        
-        public TreeLevel(TreeNode t, int l){
-            this.tree = t;
-            this.level = l;
-        }
     }
 }
