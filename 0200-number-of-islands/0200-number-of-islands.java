d240a7d896428fc061ca89d4b9beb8b15ac0042d
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-09 14:05:22 -0800

    Time: 9 ms (9.77%), Space: 51.5 MB (33.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,70 @@
 class Solution {
+    class UnionFind{
+        int count;
+        int[] parent, rank;
+
+        public UnionFind(char[][] grid){
+            count = 0;
+            int m = grid.length, n = grid[0].length;
+            this.parent = new int[m*n];
+            this.rank = new int[m*n];
+            for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    if(grid[i][j] != '0'){
+                        parent[i*n+j] = i*n+j;
+                        count++;
+                    }
+                    rank[i*n+j]=0;
+                }
+            }
+        }
+
+        public int find(int x){
+            if(parent[x]!=x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public void union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if(rootX != rootY){
+                if(rank[rootX] > rank[rootY]){
+                    parent[rootY] = rootX;
+                } else if(rank[rootX] < rank[rootY]){
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX] += 1;
+                }
+                count--;
+            }
+        }
+    }
     public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
         int m = grid.length;
         int n = grid[0].length;
-        int initX= 0, initY = 0;
-        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        int answer = 0;
-        
-        for(int i = 0 ; i < m ; i++) {
-           for(int j=0; j<n; j++) {
-               if(grid[i][j] == '1') {
-                   answer++;
-                   Queue<Point> q = new LinkedList<>();
-                   q.add(new Point(i, j));
-                   while(!q.isEmpty()){
-                       Point p = q.poll();
-                       for(int ii = 0; ii < 4 ; ii++){
-                           int nx = p.x + directions[ii][0];
-                           int ny = p.y + directions[ii][1];
-                           if(0<=nx && 0<= ny && nx < m && ny < n && grid[nx][ny] == '1'){
-                               q.add(new Point(nx, ny));
-                               grid[nx][ny] = '0';
-                           }
-                        }
+        UnionFind uf = new UnionFind(grid);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] != '0'){
+                    grid[i][j] = '0';
+                    if(i-1>=0 && grid[i-1][j]!='0'){
+                        uf.union(i*n+j, (i-i)*n+j);
                     }
-                   
-               }
-           }
+                    if(i+1<m && grid[i+1][j]!='0'){
+                        uf.union(i*n+j, (i+1)*n+j);
+                    }
+                    if(j-1>=0 && grid[i][j-1]!='0'){
+                        uf.union(i*n+j, i*n+j-1);
+                    }
+                    if(j+1<n && grid[i][j+1]!='0'){
+                        uf.union(i*n+j, i*n+j+1);
+                    }
+                }
+            }
         }
-        return answer;
-    }
-    
-    class Point {
-        int x;
-        int y;
-        
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+        return uf.count;
     }
 }
