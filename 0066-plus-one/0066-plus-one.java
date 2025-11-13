87f467d3503a234e06bf508580cabf68da5a4681
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-13 08:52:11 -0800

    Time: 0 ms (100%), Space: 43.5 MB (8.02%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for(int i=n-1; i>=0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+
+        digits = new int[n+1];
+        digits[0]=1;
+        return digits;
+    }
+}
