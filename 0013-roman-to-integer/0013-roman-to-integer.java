525660714a2dfc97391cd96dccd2c913f8fc7194
Gunhoo Park <gunhoo2016@gmail.com>
2025-11-12 20:35:00 -0800

    Time: 2 ms (100%), Space: 46.4 MB (9.79%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,67 +1,46 @@
 class Solution {
+    // MCM 1000 + 100 + (1000 - 200)
+    // IX 1 + (10 -2)
+    // XI 10 + 1
+    // MCMIV => 1000 + 100 + (1000 - 200) + 1 + (10-2)
+
     public int romanToInt(String s) {
-        int sum = 0;
-        for(int i = 0; i < s.length() ; i++ ){
-            switch(s.charAt(i)){
-                case 'I':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1) == 'X'){
-                            sum += 9;
-                            i++;
-                            break;
-                        }
-                        if(s.charAt(i+1) == 'V'){
-                            sum += 4;
-                            i++;
-                            break;
-                        }
-                    }
-                    sum += 1;
-                    break;
-                case 'V':
-                    sum += 5;
-                    break;
-                case 'X':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1) == 'L'){
-                            sum += 40;
-                            i++;
-                            break;
-                        }
-                        if(s.charAt(i+1) == 'C'){
-                            sum += 90;
-                            i++;
-                            break;
-                        }
-                    }
-                    sum += 10;
-                    break;
-                case 'L':
-                    sum += 50;
-                    break;
-                case 'C':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1) == 'D'){
-                            sum += 400;
-                            i++;
-                            break;
-                        }
-                        if(s.charAt(i+1) == 'M'){
-                            sum += 900;
-                            i++;
-                            break;
-                        }
-                    }
-                    sum += 100;
-                    break;
-                case 'D':
-                    sum += 500;
-                    break;
-                case 'M':
-                    sum += 1000;
-                    break;
-            }
+        int answer = 0;
+        char prev = s.charAt(0);
+
+        if(s.length()==1) return romanToInt(prev);
+        answer += romanToInt(prev);
+
+        for(int i=1; i<s.length(); i++){
+            char curr = s.charAt(i);
+            answer = answer + romanToInt(curr) + calculateSubstraction(curr, prev);
+            prev = curr;
         }
-        return sum;
+        return answer;
+    }
+
+    private int calculateSubstraction(char curr, char prev){
+        switch(curr){
+            case 'X': if(prev=='I') return -2; else return 0;
+            case 'V': if(prev=='I') return -2; else return 0;
+            case 'L': if(prev=='X') return -20; else return 0;
+            case 'C': if(prev=='X') return -20; else return 0;
+            case 'D': if(prev=='C') return -200; else return 0;
+            case 'M': if(prev=='C') return -200; else return 0;
+            default: return 0;
+        }
+    }
+
+    private int romanToInt(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
